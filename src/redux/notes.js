e936d7088f8125ce096a976cfa432b4e7daa3404
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-09-22 00:48:02 +0200

    Added display of notes inside main screen

--- src/redux/notes.js
+++ src/redux/notes.js
@@ -4,32 +4,64 @@ import { createSlice } from "@reduxjs/toolkit";
 //Dummy initial state
 const dummyNotes = [
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 1',
     id: '1'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 2',
     id: '2'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 3',
     id: '3'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 4',
     id: '4'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 5',
     id: '5'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 6',
     id: '6'
   },
   {
-    text: 'lorem ipsum',
+    text: 'lorem ipsum 7',
     id: '7'
+  },
+  {
+    text: 'lorem ipsum 8',
+    id: '8'
+  },
+  {
+    text: 'lorem ipsum 9',
+    id: '9'
+  },
+  {
+    text: 'lorem ipsum 10',
+    id: '10'
+  },
+  {
+    text: 'lorem ipsum 11',
+    id: '11'
+  },
+  {
+    text: 'lorem ipsum 12',
+    id: '12'
+  },
+  {
+    text: 'lorem ipsum 13',
+    id: '13'
+  },
+  {
+    text: 'lorem ipsum 14',
+    id: '14'
+  },
+  {
+    text: 'lorem ipsum 15',
+    id: '15'
   }
 ]
 
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
+
+import { useSelector } from 'react-redux';
+
+import NoteCard from '../components/NoteCard';
 
 
 const MainScreen = () => {
+  const notes = useSelector((store) => store.notes);
+
   return (
-    <View><Text>Main</Text></View>
+    <View style={styles.container}>
+      <FlatList
+        style={styles.list}
+        keyExtractor={(item) => item.id}
+        data={notes}
+        renderItem={({item}) => (
+          <NoteCard text={item.text}/>
+        )}
+      />
+    </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  list: {
+    flex: 1
+  }
+})
 
 
 export default MainScreen;
